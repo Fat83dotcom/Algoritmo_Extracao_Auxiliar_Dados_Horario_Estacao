b71c53c71d9f2e6e3a1691ff24f0fca62fe2dfe6
fat83dotcom <fernando.quimic@gmail.com>
2023-08-13 11:06:56 -0300

    Adatapção do modulo para o escopo do problema.

--- GlobalFunctions/UserEmailHandler.py
+++ GlobalFunctions/UserEmailHandler.py
@@ -9,36 +9,6 @@ class DBInterfaceConfig:
     def __init__(self, dbName: str) -> None:
         self.dbName = dbName
 
-    def executeSQL(self, sql: str, *args) -> None:
-        data = args
-        con = sqlite3.connect(self.dbName)
-        try:
-            with con:
-                con.execute(sql, *data)
-        except Exception as e:
-            raise e
-        finally:
-            con.close()
-
-    def createTableEmailRemetente(self) -> None:
-        sql = '''
-        CREATE TABLE IF NOT EXISTS email_remetente
-        (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email_remetente TEXT NOT NULL UNIQUE,
-        senha TEXT NOT NULL
-        )'''
-        self.executeSQL(sql)
-
-    def createTableEmailDestinatario(self) -> None:
-        sql = '''
-        CREATE TABLE IF NOT EXISTS email_destinatario
-        (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email_destinatario TEXT NOT NULL UNIQUE
-        )'''
-        self.executeSQL(sql)
-
     def createTableDataBase(self) -> None:
         sql = '''
         CREATE TABLE IF NOT EXISTS data_base
@@ -53,6 +23,17 @@ class DBInterfaceConfig:
         )'''
         self.executeSQL(sql)
 
+    def executeSQL(self, sql: str, *args) -> None:
+        data = args
+        con = sqlite3.connect(self.dbName)
+        try:
+            with con:
+                con.execute(sql, *data)
+        except Exception as e:
+            raise e
+        finally:
+            con.close()
+
     def select(self, sql: str) -> list:
         con = sqlite3.connect(self.dbName)
         try:
@@ -61,72 +42,19 @@ class DBInterfaceConfig:
                 result = cur.execute(sql)
                 return list(result)
         except Exception as e:
-            print(e)
             raise e
         finally:
             con.close()
 
-    def delete(self, table: str, collumn: str, *args) -> None:
-        sql = f'DELETE FROM {table} WHERE {collumn}=?'
-        data = args
-        con = sqlite3.connect(self.dbName)
+    def selectDBData(self, dbName: str) -> dict:
         try:
-            with con:
-                con.execute(sql, data)
-        except Exception as e:
-            raise e
-        finally:
-            con.close()
-
-
-class ManipuladorDadosEmailRemetDest:
-    def __init__(self, instanciadDB: DBInterfaceConfig) -> None:
-        self.db = instanciadDB
-        self.emailRemetente = 'Sem Remetente !'
-        self.senha = 'Sem Senha !'
-
-    def __setAttr(self) -> None:
-        try:
-            sql = 'SELECT "email_remetente", "senha" FROM email_remetente'
-            dados: list = self.db.select(sql)
-            if len(dados) == 0:
-                self.emailRemetente = 'Sem Remetente !'
-                self.senha = 'Sem Senha !'
-            else:
-                self.emailRemetente = dados[0][0]
-                self.senha = dados[0][1]
-        except Exception as e:
-            raise e
-
-    def meu_email(self) -> list[str]:
-        try:
-            self.__setAttr()
-            return [self.emailRemetente]
-        except Exception as e:
-            raise e
-
-    def minha_senha(self) -> list[str]:
-        try:
-            self.__setAttr()
-            return [self.senha]
-        except Exception as e:
-            raise e
-
-
-class ManipuladorEmailDestinatario:
-    def __init__(self, instanciadDB: DBInterfaceConfig) -> None:
-        self.db = instanciadDB
-
-    def my_recipients(self) -> list[str]:
-        try:
-            sql = 'SELECT "email_destinatario" FROM email_destinatario'
-            dados: list = self.db.select(sql)
-            if len(dados) == 0:
-                destinatario: list = ['Sem Destinatário !']
-            else:
-                destinatario: list = [email[0] for email in dados]
-
-            return destinatario
+            colluns = '"db_name", "user", "host", "port", "password"'
+            sql = f'SELECT {colluns} FROM data_base WHERE nome_cadastro="{dbName}"'
+            dbChoice: list = self.select(sql)
+            keys: list = ["dbname", "user", "host", "port", "password"]
+            data: list = [dado for lista in dbChoice for dado in lista]
+            result: dict = dict(zip(keys, data))
+            return result
         except Exception as e:
             raise e
 
